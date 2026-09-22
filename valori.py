88b8db8646e297abcd0a6ea74d7942feaccc4c80
INFO_10C_CroitoruDiana <dianacroitoru924@gmail.com>
2026-09-22 10:17:03 +0300

    Add files via upload

---
+++ valori.py
@@ -0,0 +1,7 @@
+import math
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+print('Valoarea maxima este',max(a,b,c))
+print('Valoarea minima este',min(a,b,c))
+print('Valoarea absoluta dintre',a, 'si', b, 'este',abs(a-b))
