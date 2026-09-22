e9faeca15a0987b24199d857c77aae80e2944596
INFO_10C_CroitoruDiana <dianacroitoru924@gmail.com>
2026-09-22 10:01:18 +0300

    Add files via upload

---
+++ INFO_DianaCroitoru.py
@@ -0,0 +1,6 @@
+lungime=float(input('lungime='))
+latime= float(input('latime='))
+aria= lungime*latime
+perimetru=2*(lungime+latime)
+print('aria dreptunghiului=', round(aria,2))
+print('perimetrul dreptunghiului=',round(perimetru,2))
